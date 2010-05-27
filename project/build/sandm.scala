d946193a77f5b51edb5dad8ce96ede0909dc23a1
11nc <lincoln@hotpotato.com>
2010-05-27 11:31:05 -0400

    cleaned up dependencies

--- project/build/sandm.scala
+++ project/build/sandm.scala
@@ -5,10 +5,6 @@ class SandMProject(info: ProjectInfo) extends DefaultProject(info) {
   
 	override def compileOptions = Deprecation :: ExplainTypes :: Unchecked :: super.compileOptions.toList
 
-  // val specs = "org.scala-tools.testing" % "specs" % "1.6.2.1" % "test->default"
-
-  // val configgyrepo = "Configgy's Repository" at "http://www.lag.net/repo"
-  // val configgy = "net.lag" % "configgy" % "1.5"
   val mongo = "org.mongodb" % "mongo-java-driver" % "1.4"
 	
 }
