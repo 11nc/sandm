efa78b7ee97996ef577daa1fb5d491f4c5139b7a
11nc <lincoln@hotpotato.com>
2010-05-27 11:54:58 -0400

    publishing docs and sources

--- project/build/sandm.scala
+++ project/build/sandm.scala
@@ -2,6 +2,12 @@ import sbt._
 import Process._
 
 class SandMProject(info: ProjectInfo) extends DefaultProject(info) {
+
+  override def packageDocsJar = defaultJarPath("-javadoc.jar")
+  override def packageSrcJar= defaultJarPath("-sources.jar")
+  val sourceArtifact = Artifact.sources(artifactID)
+  val docsArtifact = Artifact.javadoc(artifactID)
+  override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageDocs, packageSrc)
   
 	override def compileOptions = Deprecation :: ExplainTypes :: Unchecked :: super.compileOptions.toList
 
